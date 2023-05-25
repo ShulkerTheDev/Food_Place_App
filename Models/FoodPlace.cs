314bce2946fbb9adf8bd58bceef6ef48e42830d2
Devaughn Thompson <42242487+ShulkerTheDev@users.noreply.github.com>
2023-05-25 11:09:13 -0400

    Update FoodPlace.cs

--- Models/FoodPlace.cs
+++ Models/FoodPlace.cs
@@ -1,6 +1,6 @@
 public class FoodPlace
 {
-    public string Name { get; set; }
+    public string? Name { get; set; }
     public int DistanceRating { get; set; }
     public int PriceRating { get; set; }
     public int HealthRating { get; set; }
